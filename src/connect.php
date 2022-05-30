a3a062dc8f752df7f337284f78c407123f089dbd
Adinda Tasya Amelia <94682489+adindata10@users.noreply.github.com>
2022-05-30 23:09:10 +0700

    Initial commit

---
+++ src/connect.php
@@ -0,0 +1,21 @@
+<?php
+    $jkelamin = $_POST['jkelamin'];
+    $usia = $_POST['usia'];
+    $pekerjaan = $_POST['pekerjaan'];
+    $sarankritik = $_POST['sarankritik'];
+
+    //database connection
+    $conn = new mysqli('localhost','root','','crochetnknit');
+    if($conn->connect_error){
+        echo "$conn->connect_error";
+        die("Connection Failed : ". $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("insert into sarandankritik(jkelamin, usia, pekerjaan, sarankritik) values(?, ?, ?, ?)");
+        $stmt->bind_param("siss", $jkelamin, $usia, $pekerjaan, $sarankritik);
+        $execval = $stmt->execute();
+        echo $execval;
+        echo "Terima Kasih";
+        $stmt->close();
+        $conn->close();
+    }
+?>
